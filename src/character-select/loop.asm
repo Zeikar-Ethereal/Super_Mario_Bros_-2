738aa7ba8201d928a71481f7a71a1e8be704e941
Producks <davetrap.demers@gmail.com>
2025-03-07 11:14:02 -0500

    color sdone

--- src/character-select/loop.asm
+++ src/character-select/loop.asm
@@ -23,10 +23,10 @@ SetCursorLocationGFXCharSelectPTwo:
   AND #$0F
   LSR A
   LSR A
-  TAX
+  TAX ; Get these bits XXXX 00XXX
   LDA CursorLocationPTwo
   AND #$03
-  TAY
+  TAY ; Extract the least significant nibble
   LDA PlayerSelectPLetterX, Y
   STA SpriteDMAArea + 11
   LDA PlayerSelectPNumberX, Y
@@ -36,6 +36,13 @@ SetCursorLocationGFXCharSelectPTwo:
   STA SpriteDMAArea + 12
   RTS
 
+; ------------------------------------------------------------
+; Extract the MSN from the cursor, which contain the previous
+; cursor location, and use that to put the sprite at that
+; index to palette 0.
+; Params:
+;         A = Cursor location
+; ------------------------------------------------------------
 BlackOutPrevChar:
   LSR A
   LSR A
@@ -52,21 +59,41 @@ BlackOutPrevChar:
   STA SpriteDMAArea + 12, Y
   RTS
 
+; ------------------------------------------------------------
+; This function update the sprite that the cursor is pointing
+; at and give it the correct palette according to X.
+; Using X here help us reuse this function for both P1 and P2.
+; Params:
+;         A = The cursor value
+;         X = Either #$01 P1 or #$02 P2
+; ------------------------------------------------------------
 UpdatePaletteCharacter:
   AND #$0F
   TAY
   LDA DMATableCharacterPalette, Y
   TAY
-  LDA #$01
+  TXA
   STA SpriteDMAArea, Y
   STA SpriteDMAArea + 8, Y
-  LDA #$41
+  TXA
+  CLC
+  ADC #$40
   STA SpriteDMAArea + 4, Y
   STA SpriteDMAArea + 12, Y
   RTS
 
-; Loop unrolling! Also make it so I can save 3 bytes per palette
+; ------------------------------------------------------------
+; Update palette slot 1 with the cursor location.
+; Will update slot 2 with the second cursor if a 2 player mode
+; was selected. There was no good way to split this subroutine.
+; Params:
+;         CursorLocation (RAM)
+;         TwoPlayerCharacterSelect (RAM)
+;         CursorLocationPTwo (RAM)
+; ------------------------------------------------------------
 DumpNewPaletteCharacter:
+
+; Handle slot 1 for player 1 cursor
   LDA CursorLocation
   AND #$0F
   TAY
@@ -76,8 +103,6 @@ DumpNewPaletteCharacter:
   STA PPUBuffer_301
   LDA #$14
   STA PPUBuffer_301 + 1
-  LDA #$04
-  STA PPUBuffer_301 + 2
   LDA PlayerSelectSpritePalettes, Y
 	STA PPUBuffer_301 + 3
   LDA PlayerSelectSpritePalettes + 1, Y
@@ -86,6 +111,36 @@ DumpNewPaletteCharacter:
 	STA PPUBuffer_301 + 5
   LDA PlayerSelectSpritePalettes + 3, Y
 	STA PPUBuffer_301 + 6
+
+; Check for player 2
+  LDA TwoPlayerCharacterSelect
+  BEQ FinishPlayerOneDumpPalette
+
+; Do player 2 stuff here
+  LDA CursorLocationPTwo
+  AND #$0F
+  TAY
+  LDA PlayerSelectPaletteOffsets, Y
+  TAY
+  LDA PlayerSelectSpritePalettes, Y
+	STA PPUBuffer_301 + 7
+  LDA PlayerSelectSpritePalettes + 1, Y
+	STA PPUBuffer_301 + 8
+  LDA PlayerSelectSpritePalettes + 2, Y
+	STA PPUBuffer_301 + 9
+  LDA PlayerSelectSpritePalettes + 3, Y
+	STA PPUBuffer_301 + 10
+  LDA #$00
+  STA PPUBuffer_301 + 11
+  LDA #$08 ; Set the length to 8 because of 2 player
+  STA PPUBuffer_301 + 2
+  LDA #$0B
+  STA byte_RAM_300
+  RTS
+
+FinishPlayerOneDumpPalette:
+  LDA #$04 ; Set the length to 4 because of 2 player
+  STA PPUBuffer_301 + 2
   LDA #$00
 	STA PPUBuffer_301 + 7
   LDA #$07
@@ -125,11 +180,12 @@ HandlePlayerTwoCharSelect:
   LDA CursorLocationPTwo
   JSR BlackOutPrevChar ; Black out
   LDA CursorLocationPTwo
+  LDX #$02
   JSR UpdatePaletteCharacter ; Handle character palette for player 2
-  JSR DumpNewPaletteCharacter ; Update sprite palette slot 2
 
 FinalUpdatePlayerOne:
   LDA CursorLocation
+  LDX #$01
   JSR UpdatePaletteCharacter ; Swap palette for the character
   JSR DumpNewPaletteCharacter ; Update sprite palette slot 1
 
